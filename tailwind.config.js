26bb79fb2420e43f711d61afd81892333bb56d65
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2020-04-10 09:06:48 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        paddingY: '3rem'
+      },
+      colors: {
+        'primary': '#3EBD93',
+        'gray-100': '#F0F4F8',
+        'gray-900': '#102A43',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
